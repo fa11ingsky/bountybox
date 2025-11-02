67213a71cbc4445305323b68e8300f03da587ffc
fa11ingsky <daniel.papadopoulos94@gmail.com>
2025-11-02 11:59:54 +1100

    Deployment

--- composables/1.inventory.js
+++
@@ -1,5 +0,0 @@
-import json from '~/static/inventory.json'
-
-export const getInventory = () => useState('inventory', () => json)
-export const getCart = () => useState('cart', () => new Object())
-export const getCartItems = () => useState('cartItems', () => 0)
---
+++ info/sm11-71/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1762045180190}
