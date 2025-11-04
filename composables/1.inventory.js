e469ba62aca2331e12c790c7e1fd481115395025
fa11ingsky <daniel.papadopoulos94@gmail.com>
2025-11-04 11:40:51 +1100

    added sm11 support

--- composables/1.inventory.js
+++ composables/1.inventory.js
@@ -1,5 +1,6 @@
 import json from '~/static/inventory.json'
+import sm11_json from '~/static/SM_11_inventory.json'
 
-export const getInventory = () => useState('inventory', () => json)
+export const getInventory = () => useState('inventory', () => Object.assign(json, sm11_json))
 export const getCart = () => useState('cart', () => new Object())
 export const getCartItems = () => useState('cartItems', () => 0)
