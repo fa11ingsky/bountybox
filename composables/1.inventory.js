3f057bbf8c199cdaad48d837bac9851a3c3480fc
fa11ingsky <daniel.papadopoulos94@gmail.com>
2026-09-20 00:11:39 +1000

    Added swsh5 inventory

--- composables/1.inventory.js
+++ composables/1.inventory.js
@@ -1,7 +1,8 @@
 import json from '~/static/inventory.json'
+import swsh05_json from '~/static/SWSH_05_inventory.json'
 import sm10_json from '~/static/SM_10_inventory.json'
 import sm11_json from '~/static/SM_11_inventory.json'
 
-export const getInventory = () => useState('inventory', () => Object.assign(json, sm10_json, sm11_json))
+export const getInventory = () => useState('inventory', () => Object.assign(json, swsh05_json, sm10_json, sm11_json))
 export const getCart = () => useState('cart', () => new Object())
 export const getCartItems = () => useState('cartItems', () => 0)
