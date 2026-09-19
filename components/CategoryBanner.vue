56ddebc946486ece08d02b62e1f16e9c8a0950ca
fa11ingsky <daniel.papadopoulos94@gmail.com>
2026-09-19 23:40:48 +1000

    sm10 inventory and swsh05 category added

--- components/CategoryBanner.vue
+++ components/CategoryBanner.vue
@@ -44,7 +44,7 @@ const categories = {
     pokemon: {
         name: 'Pokemon',
         subcategories: {
-            'TCG': ["Mega Evolution", "Mega Evolution - Phantasmal Flames", "Sword and Shield", "Sun and Moon - Team Up", "Sun and Moon - Unified Minds",
+            'TCG': ["Mega Evolution", "Mega Evolution - Phantasmal Flames", "Sword and Shield", "Sword and Shield - Battle Styles", "Sun and Moon - Team Up", "Sun and Moon - Unified Minds",
                 "Sun and Moon - Unbroken Bonds", "Sun and Moon - Cosmic Eclipse"
             ],
             'Accessories': ["Playmats"],
--- composables/1.inventory.js
+++ composables/1.inventory.js
@@ -1,6 +1,7 @@
 import json from '~/static/inventory.json'
+import sm10_json from '~/static/SM_10_inventory.json'
 import sm11_json from '~/static/SM_11_inventory.json'
 
-export const getInventory = () => useState('inventory', () => Object.assign(json, sm11_json))
+export const getInventory = () => useState('inventory', () => Object.assign(json, sm10_json, sm11_json))
 export const getCart = () => useState('cart', () => new Object())
 export const getCartItems = () => useState('cartItems', () => 0)
