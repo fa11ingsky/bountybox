3ba5cca541af8afbf4caa121b3532f4d743a249e
fa11ingsky <daniel.papadopoulos94@gmail.com>
2026-09-20 15:40:28 +1000

    jtg, base, jungle, me1 sets added, me2 updated with stamped card,

--- components/CategoryBanner.vue
+++ components/CategoryBanner.vue
@@ -45,7 +45,7 @@ const categories = {
         name: 'Pokemon',
         subcategories: {
             'TCG': ["Mega Evolution", "Mega Evolution - Phantasmal Flames", "Sword and Shield", "Sword and Shield - Battle Styles", "Sun and Moon - Team Up", "Sun and Moon - Unified Minds",
-                "Sun and Moon - Unbroken Bonds", "Sun and Moon - Cosmic Eclipse"
+                "Sun and Moon - Unbroken Bonds", "Sun and Moon - Cosmic Eclipse", "Scarlet and Violet - Journey Together", "Base Set", "Jungle"
             ],
             'Accessories': ["Playmats"],
             'Figures': ["Coming Soon!"]
