d25547511e8e409667b87f45a014e273913d091c
fa11ingsky <daniel.papadopoulos94@gmail.com>
2026-09-20 11:18:49 +1000

    Add phantasmal flame set

--- composables/1.inventory.js
+++ composables/1.inventory.js
@@ -2,7 +2,8 @@ import json from '~/static/inventory.json'
 import swsh05_json from '~/static/SWSH_05_inventory.json'
 import sm10_json from '~/static/SM_10_inventory.json'
 import sm11_json from '~/static/SM_11_inventory.json'
+import me2_json from '~/static/ME2_inventory.json'
 
-export const getInventory = () => useState('inventory', () => Object.assign(json, swsh05_json, sm10_json, sm11_json))
+export const getInventory = () => useState('inventory', () => Object.assign(json, swsh05_json, sm10_json, sm11_json, me2_json))
 export const getCart = () => useState('cart', () => new Object())
 export const getCartItems = () => useState('cartItems', () => 0)
